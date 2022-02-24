641117ea68620e16907f15e665011dc1bb09c87c
Shreya <dhirshreya@gmail.com>
2022-02-24 13:32:56 +0530

    upgrade dependencies

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
-console.log(" world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const main = async () => {
+    const orm = await core_1.MikroORM.init()({
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: process.env.NODE_DEV === 'production'
+    });
+};
+console.log("hello world");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(" world")
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async () => {
+  const orm = await MikroORM.init()({
+    dbName: 'lireddit',
+    type: 'postgresql',
+    debug: process.env.NODE_DEV === 'production' // true in dev
+  });
+};
+
+// 1. creating mikro-orm instance
+// 2. asynchronise the GET request
+
+console.log("hello world")
